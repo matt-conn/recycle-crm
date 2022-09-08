f96b47bdac370cdbd3b5d319b376ceb2747eff82
Matt Connolly <81105533+matt-conn@users.noreply.github.com>
2022-09-07 21:41:00 -0400

    initial commit

---
+++ models/Company.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const CompanySchema = new mongoose.Schema({
+	todo: {
+		type: String,
+		required: true,
+  	},
+	
+  	// completed: {
+	// 	type: Boolean,
+	// 	required: true,
+  	// }
+})
+
+module.exports = mongoose.model('Company', TodoSchema)
